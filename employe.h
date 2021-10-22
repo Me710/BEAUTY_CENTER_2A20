1cacdca8b2c44e7374a874adf53e87fb4799e185
CHRISTIAN PARFAIT <christianparfait.nebotfonkou@esprit.tn>
2021-10-22 12:06:18 +0100

    ok

--- employe.h
+++ employe.h
@@ -1,19 +1,37 @@
 #ifndef EMPLOYE_H
 #define EMPLOYE_H
+#include <QString>
+#include <iostream>
 
+using namespace std;
 
-/*class Employe
+class Employe
 {
 public:
-    Employe();
+    QString cin;
+    QString prenom;
+    QString nom;
+    QString courrier;
+    int tel;
+    double revenue;
+    char sex;
+    QString adresse;
+    int jourNais;
+    int moisNais;
+    int anneeNais;
+
+private:
+     Employe(){}
+    ~Employe(){}
+    void setcin(QString n){cin=n;}
+    void setprenom(QString n){prenom=n;}
+    void setnom(QString n){nom=n;}
+
+    QString get_cin(){return cin;}
+    QString get_prenom(){return prenom;}
+    QString get_nom(){return nom;}
 };
 
-void Client::setcin(QString n){cin=n;}
-void Client::setprenom(QString n){prenom=n;}
-void Client::setnom(QString n){nom=n;}
 
-QString Client::get_cin(){return cin;}
-QString Client::get_prenom(){return prenom;}
-QString Client::get_nom(){return nom;}*/
 
 #endif // EMPLOYE_H
